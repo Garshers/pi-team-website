9d05189e83b9d105323e07482ebc797ace65dda2
Krzysztof <krzysztof.pawik@gmail.com>
2025-05-11 21:54:27 +0200

    Creating loading images for HomePageGallery

--- src/components/Gallery/HomePageGallery.js
+++ src/components/Gallery/HomePageGallery.js
@@ -1,21 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './galleryStyle.css';
 import gymImage from '../../assets/HomePage3.jpeg';
-import poolImage from '../../assets/HomePage1.jpeg'
+import gymImage_Small from '../../assets/HomePage3_Small.jpeg';
+import poolImage from '../../assets/HomePage1.jpeg';
+import poolImage_Small from '../../assets/HomePage1_Small.jpeg';
+import courseImage from '../../assets/HomePage3_copy.jpeg';
+import courseImage_Small from '../../assets/HomePage3_copy_Small.jpeg';
+import campImage from '../../assets/HomePage1_copy.jpeg';
+import campImage_Small from '../../assets/HomePage1_copy_Small.jpeg';
 
 function HomePageGallery() {
+    const [loadedStates, setLoadedStates] = useState([false, false, false, false]);
+
+    const handleImageLoad = (index) => {
+        const newLoadedStates = [...loadedStates];
+        newLoadedStates[index] = true;
+        console.log("Loaded index: ", index, "(", newLoadedStates[index], ")");
+        setLoadedStates(newLoadedStates);
+    };
+
     return (
         <div className='galleryMainFrame'>
             <div className='galleryRow'>
                 <a href={'/'} className='galleryBox'>
-                    <img src={gymImage} alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
+                    <div
+                        className="blurred-img"
+                        style={{ backgroundImage: `url(${gymImage_Small})`, filter: loadedStates[0] ? 'none' : 'blur(5px)' }}
+                    >
+                        <img
+                            src={gymImage}
+                            alt='siłownia'
+                            className='galleryImageStyle'
+                            loading="lazy"
+                            key={'gym1'}
+                            onLoad={() => handleImageLoad(0)}
+                            style={{ opacity: loadedStates[0] ? 1 : 0 }}
+                        />
+                        {console.log('Ścieżka obrazu 0:', loadedStates[0] ? gymImage : gymImage_Small)}
+                    </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Siłownia</div>
                         <div className='galleryBoxName__secondary'>Chcesz, by Twoja determinacja, charakter i upór była zauważana i interpretowana już przez pryzmat samego wyglądu? Jak bardzo wygląd przekłada się na postrzeganie Ciebie jako osoby? Zapraszamy do współpracy</div>
                     </div>
                 </a>
                 <a href={'/'} className='galleryBox'>
-                    <img src={poolImage} alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
+                    <div
+                        className="blurred-img"
+                        style={{ backgroundImage: `url(${poolImage_Small})`, filter: loadedStates[1] ? 'none' : 'blur(5px)' }}
+                    >
+                        <img
+                            src={poolImage}
+                            alt='basen'
+                            className='galleryImageStyle'
+                            loading="lazy"
+                            key={'pool1'}
+                            onLoad={() => handleImageLoad(1)}
+                            style={{ opacity: loadedStates[1] ? 1 : 0 }}
+                        />
+                    </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Basen</div>
                         <div className='galleryBoxName__secondary'>Chcesz się czuć jak ryba w wodzie dosłownie i w przenośni? Lepiej nie trafisz!</div>
@@ -24,14 +66,40 @@ function HomePageGallery() {
             </div>
             <div className='galleryRow'>
                 <a href={'/'} className='galleryBox'>
-                    <img src={gymImage} alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
+                    <div
+                        className="blurred-img"
+                        style={{ backgroundImage: `url(${courseImage_Small})`, filter: loadedStates[2] ? 'none' : 'blur(5px)' }}
+                    >
+                        <img
+                            src={courseImage}
+                            alt='Kurs Trenera Personalnego'
+                            className='galleryImageStyle'
+                            loading="lazy"
+                            key={'course'}
+                            onLoad={() => handleImageLoad(2)}
+                            style={{ opacity: loadedStates[2] ? 1 : 0 }}
+                        />
+                    </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Obozy Sportowe</div>
                         <div className='galleryBoxName__secondary'>Wakacje, które robią formę - sportowy reset i piękna pogoda.</div>
                     </div>
                 </a>
                 <a href={'/'} className='galleryBox'>
-                    <img src={poolImage} alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
+                    <div
+                        className="blurred-img"
+                        style={{ backgroundImage: `url(${campImage_Small})`, filter: loadedStates[3] ? 'none' : 'blur(5px)' }}
+                    >
+                        <img
+                            src={campImage}
+                            alt='Obozy Sportowe'
+                            className='galleryImageStyle'
+                            loading="lazy"
+                            key={'camp'}
+                            onLoad={() => handleImageLoad(3)}
+                            style={{ opacity: loadedStates[3] ? 1 : 0 }}
+                        />
+                    </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Kurs Trenera Personalnego</div>
                         <div className='galleryBoxName__secondary'>Marzysz o pracy, która łączy sport, rozwój i poczucie satysfakcji? Zostań trenerem personalnym! Potężna dawka nowoczesnej wiedzy, działającej praktyki i skutecznej motywacji. Nauczysz się, jak planować treningi, budować relacje z klientem i być najlepszym w swoim fachu. Tylko z PITEAM.</div>
@@ -43,46 +111,3 @@ function HomePageGallery() {
 }
 
 export default HomePageGallery;
-
-/*
-function HomePageGallery() {
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 700);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 700);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return (
-        <div className='galleryMainFrame'>
-            <div className='galleryRow'>
-                <div className='galleryBox' style={{ width: isMobile ? 'auto' : `${48}%`, padding: isMobile ? '0' : '5px 5px 0 5px',}}>
-                    <img src={'https://images.unsplash.com/photo-1742155032253-744e8adaafc8?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} 
-                        alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
-                    <a href={'/'} className='galleryButtonLink'>Zobacz więcej</a>
-                </div>
-                <div className='galleryBox' style={{ width: isMobile ? 'auto' : `${52}%`, padding: isMobile ? '0' : '5px 5px 0 0',}}>
-                    <img src={'https://images.unsplash.com/photo-1741851374721-a546dc41561a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} 
-                        alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
-                    <a href={'/'} className='galleryButtonLink'>Zobacz więcej</a>
-                </div>
-            </div>
-            <div className='galleryRow'>
-                <div className='galleryBox' style={{ width: isMobile ? 'auto' : `${55}%`, padding: isMobile ? '0' : '5px 5px 0 5px',}}>
-                    <img src={'https://images.unsplash.com/photo-1742155032253-744e8adaafc8?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} 
-                        alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
-                    <a href={'/'} className='galleryButtonLink'>Zobacz więcej</a>
-                </div>
-                <div className='galleryBox' style={{ width: isMobile ? 'auto' : `${45}%`, padding: isMobile ? '0' : '5px 5px 0 0',}}>
-                    <img src={'https://images.unsplash.com/photo-1741851374721-a546dc41561a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'} 
-                        alt='lena' className='galleryImageStyle' loading="lazy" key={'lena'} />
-                    <a href={'/'} className='galleryButtonLink'>Zobacz więcej</a>
-                </div>
-            </div>
-        </div>
-    );
-}*/
