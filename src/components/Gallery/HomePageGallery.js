c08de4c7670c66b4717ddeb6709f02634fc2923d
Krzysztof <krzysztof.pawik@gmail.com>
2025-05-15 20:04:39 +0200

    Fix: Incorrectly tracking loaded images due to state overwritingProblem:The `handleImageLoad` function was incorrectly updating the `loadedStates` state. Each invocation relied on a stale state value, causing information about previously loaded images to be lost. As a result, the state did not accurately reflect which images had been loaded.Solution:The method of updating the `loadedStates` state in the `handleImageLoad` function was changed to use the functional form of `setLoadedStates`. This ensures that the new state value is calculated based on the previous state, allowing for the correct accumulation of information about loaded images.

--- src/components/Gallery/HomePageGallery.js
+++ src/components/Gallery/HomePageGallery.js
@@ -1,23 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './galleryStyle.css';
 import gymImage from '../../assets/HomePage3.jpeg';
 import gymImage_Small from '../../assets/HomePage3_Small.jpeg';
 import poolImage from '../../assets/HomePage1.jpeg';
 import poolImage_Small from '../../assets/HomePage1_Small.jpeg';
-import courseImage from '../../assets/HomePage3_copy.jpeg';
-import courseImage_Small from '../../assets/HomePage3_copy_Small.jpeg';
-import campImage from '../../assets/HomePage1_copy.jpeg';
-import campImage_Small from '../../assets/HomePage1_copy_Small.jpeg';
+import courseImage from '../../assets/HomePage3.jpeg';
+import courseImage_Small from '../../assets/HomePage3_Small.jpeg';
+import campImage from '../../assets/HomePage1.jpeg';
+import campImage_Small from '../../assets/HomePage1_Small.jpeg';
 
 function HomePageGallery() {
     const [loadedStates, setLoadedStates] = useState([false, false, false, false]);
+    const secondaryTextRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+    const [isVisible, setIsVisible] = useState([false, false, false, false]);
 
     const handleImageLoad = (index) => {
-        const newLoadedStates = [...loadedStates];
+    setLoadedStates(prevLoadedStates => {
+        const newLoadedStates = [...prevLoadedStates];
         newLoadedStates[index] = true;
-        console.log("Loaded index: ", index, "(", newLoadedStates[index], ")");
-        setLoadedStates(newLoadedStates);
+        return newLoadedStates;
+    });
+    console.log(`Image`, index, `loaded (after setState):`, loadedStates);
     };
+
+    useEffect(() => {
+        console.log('Current loadedStates:', loadedStates);
+    }, [loadedStates]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    const index = secondaryTextRefs.findIndex(ref => ref.current === entry.target);
+                    if (index !== -1 && entry.isIntersecting) {
+                        const newIsVisible = [...isVisible];
+                        newIsVisible[index] = true;
+                        setIsVisible(newIsVisible);
+                    }
+                });
+            },
+            {
+                threshold: 0.01,
+            }
+        );
+
+        secondaryTextRefs.forEach(ref => {
+            if (ref.current) {
+                observer.observe(ref.current);
+            }
+        });
+
+        return () => {
+            observer.disconnect();
+        };
+    }, [isVisible]);
 
     return (
         <div className='galleryMainFrame'>
@@ -25,42 +61,45 @@ function HomePageGallery() {
                 <a href={'/'} className='galleryBox'>
                     <div
                         className="blurred-img"
-                        style={{ backgroundImage: `url(${gymImage_Small})`, filter: loadedStates[0] ? 'none' : 'blur(10px)' }}
+                        style={{ backgroundImage: `url(${gymImage_Small})`, filter: loadedStates[0] ? 'none' : 'blur(7px)' }}
                     >
                         <img
                             src={gymImage}
                             alt='siłownia'
-                            className='galleryImageStyle'
                             loading="lazy"
-                            key={'gym1'}
-                            onLoad={() => handleImageLoad(0)}
+                            key={0}
+                            onLoad={() => {console.log('onLoad triggered for index: 0'); handleImageLoad(0);}}
+                            onError={() => console.error("Błąd ładowania obrazu:", gymImage)}
                             style={{ opacity: loadedStates[0] ? 1 : 0 }}
                         />
-                        {console.log('Ścieżka obrazu 0:', loadedStates[0] ? gymImage : gymImage_Small)}
                     </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Siłownia</div>
-                        <div className='galleryBoxName__secondary'>Chcesz, by Twoja determinacja, charakter i upór była zauważana i interpretowana już przez pryzmat samego wyglądu? Jak bardzo wygląd przekłada się na postrzeganie Ciebie jako osoby? Zapraszamy do współpracy</div>
+                        <div className={`galleryBoxName__secondary ${isVisible[1] ? 'animate' : ''}`} ref={secondaryTextRefs[1]}>
+                            Chcesz, by Twoja determinacja, charakter i upór była zauważana i interpretowana już przez pryzmat samego wyglądu? Jak bardzo wygląd przekłada się na postrzeganie Ciebie jako osoby? Zapraszamy do współpracy
+                        </div>
                     </div>
                 </a>
                 <a href={'/'} className='galleryBox'>
                     <div
                         className="blurred-img"
-                        style={{ backgroundImage: `url(${poolImage_Small})`, filter: loadedStates[1] ? 'none' : 'blur(10px)' }}
+                        style={{ backgroundImage: `url(${poolImage_Small})`, filter: loadedStates[1] ? 'none' : 'blur(7px)' }}
                     >
                         <img
                             src={poolImage}
                             alt='basen'
-                            className='galleryImageStyle'
                             loading="lazy"
-                            key={'pool1'}
+                            key={1}
                             onLoad={() => handleImageLoad(1)}
+                            onError={() => console.error("Błąd ładowania obrazu:", poolImage)}
                             style={{ opacity: loadedStates[1] ? 1 : 0 }}
                         />
                     </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Basen</div>
-                        <div className='galleryBoxName__secondary'>Chcesz się czuć jak ryba w wodzie dosłownie i w przenośni? Lepiej nie trafisz!</div>
+                        <div className={`galleryBoxName__secondary ${isVisible[1] ? 'animate' : ''}`} ref={secondaryTextRefs[1]}>
+                            Chcesz się czuć jak ryba w wodzie dosłownie i w przenośni? Lepiej nie trafisz!
+                        </div>
                     </div>
                 </a>
             </div>
@@ -68,41 +107,43 @@ function HomePageGallery() {
                 <a href={'/'} className='galleryBox'>
                     <div
                         className="blurred-img"
-                        style={{ backgroundImage: `url(${courseImage_Small})`, filter: loadedStates[2] ? 'none' : 'blur(10px)' }}
+                        style={{ backgroundImage: `url(${courseImage_Small})`, filter: loadedStates[2] ? 'none' : 'blur(7px)' }}
                     >
                         <img
                             src={courseImage}
                             alt='Kurs Trenera Personalnego'
-                            className='galleryImageStyle'
                             loading="lazy"
-                            key={'course'}
+                            key={2}
                             onLoad={() => handleImageLoad(2)}
                             style={{ opacity: loadedStates[2] ? 1 : 0 }}
                         />
                     </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Obozy Sportowe</div>
-                        <div className='galleryBoxName__secondary'>Wakacje, które robią formę - sportowy reset i piękna pogoda.</div>
+                        <div className={`galleryBoxName__secondary ${isVisible[2] ? 'animate' : ''}`} ref={secondaryTextRefs[2]}>
+                            Wakacje, które robią formę - sportowy reset i piękna pogoda.
+                        </div>
                     </div>
                 </a>
                 <a href={'/'} className='galleryBox'>
                     <div
                         className="blurred-img"
-                        style={{ backgroundImage: `url(${campImage_Small})`, filter: loadedStates[3] ? 'none' : 'blur(10px)' }}
+                        style={{ backgroundImage: `url(${campImage_Small})`, filter: loadedStates[3] ? 'none' : 'blur(7px)' }}
                     >
                         <img
                             src={campImage}
                             alt='Obozy Sportowe'
-                            className='galleryImageStyle'
                             loading="lazy"
-                            key={'camp'}
+                            key={3}
                             onLoad={() => handleImageLoad(3)}
                             style={{ opacity: loadedStates[3] ? 1 : 0 }}
                         />
                     </div>
                     <div className='galleryBoxName'>
                         <div className='galleryBoxName__primary'>Kurs Trenera Personalnego</div>
-                        <div className='galleryBoxName__secondary'>Marzysz o pracy, która łączy sport, rozwój i poczucie satysfakcji? Zostań trenerem personalnym! Potężna dawka nowoczesnej wiedzy, działającej praktyki i skutecznej motywacji. Nauczysz się, jak planować treningi, budować relacje z klientem i być najlepszym w swoim fachu. Tylko z PITEAM.</div>
+                        <div className={`galleryBoxName__secondary ${isVisible[3] ? 'animate' : ''}`} ref={secondaryTextRefs[3]}>
+                            Marzysz o pracy, która łączy sport, rozwój i poczucie satysfakcji? Zostań trenerem personalnym! Potężna dawka nowoczesnej wiedzy, działającej praktyki i skutecznej motywacji. Nauczysz się, jak planować treningi, budować relacje z klientem i być najlepszym w swoim fachu. Tylko z PITEAM.
+                        </div>
                     </div>
                 </a>
             </div>
